b0f00b4623d89437082e91af842eae40a084bda6
Takezo Miyamoto <takezo.m.1987@gmail.com>
2016-12-27 11:29:24 +0900

    validation in listing model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,10 @@
 class Listing < ActiveRecord::Base
   belongs_to :user
+
+  #必須項目
+  validates :home_type, precence: true
+  validates :pet_type, precence: true
+  validates :pet_size, precence: true
+  validates :breeding_years, precence: true
+
 end

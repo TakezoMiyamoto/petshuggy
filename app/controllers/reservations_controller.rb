d58e494a5609d8adaa28325c0a4624faea36b0aa
Takezo Miyamoto <takezo.m.1987@gmail.com>
2016-12-29 22:02:47 +0900

    edited calender view in listing

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,9 +1,45 @@
 class ReservationsController < ApplicationController
-  
-  def create
-    @reservation = current_user.reservations.create(reservation_params)
 
-    redirect_to @reservation.listing, notice:"予約が完了しました"
+
+  def create
+    @listing = Listing.find(params[:listing_id])
+
+    # 自分で自分の部屋を予約する場合(つまり預かれへん日を設定したい場合)
+    if current_user = @listing.user
+      # 選択された日付を","で区切って配列かする
+      selectedDates = params[:reservation][:selectedDates].split(",")
+      # 自分で予約した予約を取り出して変数に打ち込む
+      reservationsByme = @listing.reservations.where(user_id: current_user.id)
+
+      # 以前に自分で予約した日付の配列
+      oldSelectedDates = []
+
+      reservationsByme.each do |reservation|
+        oldSelectedDates.push(reservation.start_date)
+      end
+
+      # 以前に自分で予約した予約を全て一旦消す！
+      if oldSelectedDates
+        oldSelectedDates.each do |date|
+          @reservation = current_user.reservations.where(start_date:date, end_date:date)
+          @reservation.destroy_all
+        end
+      end
+
+      selectedDates
+      if selectedDates
+        selectedDates.each do |date|
+          current_user.reservations.create(:listing_id => @listing.id, :start_date => date, :end_date => date)
+        end
+      end
+
+      redirect_to :back, notice: "更新しました"
+    else
+      # 他人のやつに予約したい時。予約をパラメータ付与して作成
+      @reservations = current_user.reservations.create(reservation_params)
+      redirect_to @reservation.listing, notice: "予約が完了しました。"
+    end
+
   end
 
   def setdate
